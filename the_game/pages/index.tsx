2fc9d663c31ed7940dd977980aa3bdad09489b82
Cristian Bucurenciu <bucurenciu.cristian@gmail.com>
2022-10-18 16:13:22 +0200

    Testting

--- the_game/pages/index.tsx
+++ the_game/pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          Incredible <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
