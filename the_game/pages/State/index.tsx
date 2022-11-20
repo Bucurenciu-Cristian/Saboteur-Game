ff64bff43ad6170d6ef352dd350606e685d47726
Bucurenciu Bogdan Cristian <cristian.bucurenciu@sepa.at>
2022-11-20 13:34:49 +0100

    UseMemoExample

--- the_game/pages/State/index.tsx
+++ the_game/pages/State/index.tsx
@@ -2,14 +2,22 @@
 import StateUsePage from "../../src/components/State/useState";
 import StatusUseReducer from "../../src/components/State/useReducer";
 import StatusUseEffect from "../../src/components/State/useEffect";
+import StatusUseMemo from "../../src/components/State/useMemo";
+const RenderLine = () => <><br/><hr/><br/></>
 const App = () => {
   // const {pokemon} = usePokemon();
 
   return (<div>
     {/*{JSON.stringify(pokemon)}*/}
-    {/*<StateUsePage />*/}
-    {/*<StatusUseReducer/>*/}
+
+    <RenderLine/>
+    <StateUsePage />
+    <RenderLine/>
+    <StatusUseReducer/>
+    <RenderLine/>
     <StatusUseEffect/>
+    <RenderLine/>
+    <StatusUseMemo/>
   </div>);
 };
 export default App;
