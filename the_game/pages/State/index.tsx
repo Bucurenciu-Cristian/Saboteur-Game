7ab88b85e2f3eba1296261e0876aed7c461a2bdc
Bucurenciu Bogdan Cristian <cristian.bucurenciu@sepa.at>
2022-11-20 13:02:42 +0100

    UseEffectExample

--- the_game/pages/State/index.tsx
+++ the_game/pages/State/index.tsx
@@ -1,12 +1,15 @@
 // import usePokemon from "../../src/components/State/Context";
 import StateUsePage from "../../src/components/State/useState";
+import StatusUseReducer from "../../src/components/State/useReducer";
+import StatusUseEffect from "../../src/components/State/useEffect";
 const App = () => {
   // const {pokemon} = usePokemon();
 
   return (<div>
     {/*{JSON.stringify(pokemon)}*/}
-    <StateUsePage />
-
+    {/*<StateUsePage />*/}
+    {/*<StatusUseReducer/>*/}
+    <StatusUseEffect/>
   </div>);
 };
 export default App;
