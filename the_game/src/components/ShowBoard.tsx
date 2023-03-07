38b353ba0e27d1d9b895b0a948dbf23e26b371ee
Kicky <bucurenciu.cristian@gmail.com>
2023-03-07 18:59:10 +0200

    Memoized the Board from Table Page

--- the_game/src/components/ShowBoard.tsx
+++ the_game/src/components/ShowBoard.tsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, {useMemo} from "react";
 import Square from "./Square";
 
-// @ts-ignore
-const ShowBoard = ({gameMatrix}) => {
+const ShowBoard = ({gameMatrix}: { gameMatrix: any[][] }) => {
 
-  const renderSquareUpdated = (i: React.Key , j: number) => {
-    return <Square
-      value={gameMatrix[i][j]}
-      onClick={() => console.log(`Locatia este: [${i},${j}]`)}
-    />;
-  };
-  return <>{gameMatrix.map((row: any[], i: React.Key) => {
+    const renderSquareUpdated = (i: number, j: number) => {
+        return (
+            <Square
+                value={gameMatrix[i][j]?.Card}
+                onClick={() => console.log(`Locatia este: [${i},${j}]`)}
+            />
+        );
+    };
+
+    const memoizedGameMatrix = useMemo(() => gameMatrix, [gameMatrix]);
+
+    const memoizedRenderSquareUpdated = useMemo(
+        () => renderSquareUpdated,
+        [gameMatrix]
+    );
+
     return (
-      <div key={i} className="board-row">
-        {row.map((column, j) => {
-          return (
-            <div key={`${i}${j}`}>
-              {renderSquareUpdated(i, j)}
-            </div>
-          );
-        })}
-      </div>)
-  })}</>;
-}
+        <>
+            {memoizedGameMatrix.map((row: any[], i) => {
+                return (
+                    <div key={i} className="board-row">
+                        {row.map((column, j) => {
+                            return (
+                                <div key={`${i}${j}`}>
+                                    {memoizedRenderSquareUpdated(i, j)}
+                                </div>
+                            );
+                        })}
+                    </div>
+                );
+            })}
+        </>
+    );
+};
+
 export default ShowBoard;
