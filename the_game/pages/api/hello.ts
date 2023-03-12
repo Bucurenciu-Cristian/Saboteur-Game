81b84ffb59c5112538a189424e4d95216a56a191
Kicky <bucurenciu.cristian@gmail.com>
2023-03-12 20:09:10 +0200

    Basic Server API Stuff

--- the_game/pages/api/hello.ts
+++ the_game/pages/api/hello.ts
@@ -1,13 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type {NextApiRequest, NextApiResponse} from 'next'
 
 type Data = {
-  name: string
+    name: string
 }
 
 export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'Salut din Api' })
+    if (req.method === 'POST') {
+        res.status(200).json({name: 'POST request'})
+    } else if (req.method === 'GET') {
+        res.status(200).json({name: 'GET request'})
+    } else if (req.method === 'PUT') {
+        res.status(200).json({name: 'PUT request'})
+    } else if (req.method === 'DELETE') {
+        res.status(200).json({name: 'DELETE request'})
+    }
+
 }
