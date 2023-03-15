42fdcffb7778e1af8cf9fde0f354f5bb4817a5b0
Kicky <bucurenciu.cristian@gmail.com>
2023-03-15 11:59:15 +0200

    Dummy data about some 3 users

---
+++ the_game/src/BusinessLogic/Users.ts
@@ -0,0 +1,17 @@
+import {IUser} from "../Types/DexType";
+import {allPaths2} from "./Cards/Paths";
+
+export const usersList: IUser[] = [
+    {
+        UserName: 'Player1',
+        Hand: allPaths2.slice(0, 6).map(() => allPaths2[Math.floor(Math.random() * allPaths2.length)])
+    },
+    {
+        UserName: 'Player2',
+        Hand: allPaths2.slice(0, 6).map(() => allPaths2[Math.floor(Math.random() * allPaths2.length)])
+    },
+    {
+        UserName: 'Player3',
+        Hand: allPaths2.slice(0, 6).map(() => allPaths2[Math.floor(Math.random() * allPaths2.length)])
+    },
+]
