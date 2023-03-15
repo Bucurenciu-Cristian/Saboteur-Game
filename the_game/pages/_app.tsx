e2d1af5b49acc11f5186ba755f4739f4474eeb74
Kicky <bucurenciu.cristian@gmail.com>
2023-03-15 11:54:39 +0200

    Added Bootstrap in the whole website

--- the_game/pages/_app.tsx
+++ the_game/pages/_app.tsx
@@ -1,8 +1,9 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+    return <Component {...pageProps} />
 }
 
 export default MyApp
