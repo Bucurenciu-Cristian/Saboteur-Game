30d668a16b1041f16b660831b4f2aec8ac064d51
Kicky <bucurenciu.cristian@gmail.com>
2023-03-15 11:58:27 +0200

    Added some constants.ts

---
+++ the_game/src/constants.ts
@@ -0,0 +1,15 @@
+export const findCardActions = {
+    Start: 'Start',
+    Final: 'Final',
+}
+export const NeighboursActions = {
+    ONE: 'ONE',
+    ALL: 'ALL'
+}
+export const conDirections = {
+    NORTH: 'NORTH',
+    EAST: 'EAST',
+    SOUTH: 'SOUTH',
+    WEST: 'WEST',
+    CENTER: 'CENTER',
+}
