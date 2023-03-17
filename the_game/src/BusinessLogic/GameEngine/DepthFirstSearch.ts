b5c9847e1baaa016ebb91e12a737d3c30095ac74
Kicky <bucurenciu.cristian@gmail.com>
2023-03-17 19:04:28 +0200

    DepthFirstSearch.ts is working. I can see if there is one continued path from finish to start

---
+++ the_game/src/BusinessLogic/GameEngine/DepthFirstSearch.ts
@@ -0,0 +1,46 @@
+import {IMatrix} from "../../Types/DexType";
+import {isCodeAtIndex, neighboursCards} from "./Matrix";
+import {FOT} from "../Cards/Actions";
+
+export function isPathToFinish(matrix: IMatrix[][], startRow: number, startColumn: number): boolean {
+    const numRows = matrix.length;
+    const numCols = matrix[0].length;
+    const visited = new Array(numRows).fill(false).map(() => new Array(numCols).fill(false));
+
+    // Assuming the starting card is at position (0, 0).
+    return dfs(matrix, startRow, startColumn, visited);
+}
+
+export function dfs(matrix: IMatrix[][], row: number, column: number, visited: boolean[][]): boolean {
+    if (row < 0 || row >= matrix.length || column < 0 || column >= matrix[0].length) {
+        return false;
+    }
+
+    if (visited[row][column]) {
+        return false;
+    }
+    visited[row][column] = true;
+    const {code} = matrix[row][column].Card;
+
+    if (!isCodeAtIndex(matrix, row, column, 6)) {
+        return false;
+    }
+    if (code[8] === 'S') { // If we've reached a finish card.
+        return true;
+    }
+
+    const neighbours = neighboursCards(matrix, row, column);
+
+    for (const neighbour of neighbours) {
+        const {coordinate} = neighbour;
+        const {row: newRow, column: newCol} = coordinate;
+        const {center, adjacent} = neighbour;
+        if (center === FOT.T && adjacent === FOT.T) {
+            if (dfs(matrix, newRow, newCol, visited)) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
