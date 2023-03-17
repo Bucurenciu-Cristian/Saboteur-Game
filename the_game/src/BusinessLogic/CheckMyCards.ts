0c6249e8c97c0b31b85cb3343a72c9cdfda1389a
Kicky <bucurenciu.cristian@gmail.com>
2023-03-17 19:06:30 +0200

    Deleted the nonsense

--- the_game/src/BusinessLogic/CheckMyCards.ts
+++ the_game/src/BusinessLogic/CheckMyCards.ts
@@ -1,51 +1,40 @@
-import {
-  centerColumn,
-  centerRows,
-  checkTheCurrentCardInTable,
-  findAllEndCardsAlternative,
-  InitialMatrix
-} from "./GameEngine/Matrix";
+import {checkTheCurrentCardInTable, findAvailablePath, findOccupiedEdge, InitialMatrix} from "./GameEngine/Matrix";
 import {allTheCards, findTheCard} from "./Logic";
 import {TypeGuardOnCards} from "./TypeGuardOnCards";
 import {findCardActions, NeighboursActions} from "../constants";
-import {stringify} from "../../utils/Helpers";
+import {isPathToFinish} from "./GameEngine/DepthFirstSearch";
 
 export function checkMyCards() {
-  console.time('CheckingTheCards'); // start the timer
-  allTheCards.forEach((obj) => {
-    TypeGuardOnCards(obj);
-  });
-  console.timeEnd('CheckingTheCards'); // end the timer
-  /*
-    console.info(1, normalPath[0].code);
-    const code = normalPath[0].code?.join("");
-    normalPath[0].code = changeOrientation(code);
-    console.info(2, normalPath[0].code);
-  */
-  checkTheCurrentCardInTable(centerRows, centerColumn, InitialMatrix);
-  console.time('myBlock'); // start the timer
-  {
-    const startCoordonate = findTheCard(InitialMatrix, findCardActions.Start);
-    const finalCardsCoordonate = findTheCard(InitialMatrix, findCardActions.Final);
-    finalCardsCoordonate.forEach(([row, column]) => {
-      if (checkTheCurrentCardInTable(row, column, InitialMatrix, NeighboursActions.ONE)) {
-        /*//I have already the visited part in the InitialMatrix in each of the Squares.
-        // const visited: boolean[][] = Array.from({length: InitialMatrix.length}, () => new Array(InitialMatrix[0].length).fill(false));
-        // const goldCoord: Coordinate = {row: StartRow, col: lastColumn};
-        // const startCode: CharTuple | string = SpecialPath[0].code;
+    console.time('CheckingTheCards'); // start the timer
+    allTheCards.forEach((obj) => {
+        TypeGuardOnCards(obj);
+    });
+    console.timeEnd('CheckingTheCards'); // end the timer
+    console.time('StartAndFinish'); // start the timer
+    {
+        const startCoordonate = findTheCard(InitialMatrix, findCardActions.Start);
+        const finalCardsCoordonate = findTheCard(InitialMatrix, findCardActions.Final);
+        finalCardsCoordonate.forEach(([row, column]) => {
+            if (checkTheCurrentCardInTable(InitialMatrix, row, column, NeighboursActions.ONE)) {
+                console.log("Start DFS");
+                const x = isPathToFinish(InitialMatrix, row, column);
+                console.log(x);
+                console.log("END DFS");
+            }
+        });
+        startCoordonate.forEach(([row, column]) => {
+            //TODO: If you want to do something from the start, here you can do it.
+        });
 
-        // if (dfs(InitialMatrix, visited, goldCoord, startCode)) {
-        //     console.log("Found the Start card!");
-        // } else {
-        //     console.log("Could not find the Start card.");
-        // }*/
-      }
-    });
-    startCoordonate.forEach(([row, column]) => {
-      console.log('Start card found at', row, column)
-      const x = findAllEndCardsAlternative(InitialMatrix, row, column);
-      console.log('End card found at', x);
-    });
-  }
-  console.timeEnd('myBlock'); // end the timer
+        //You'll do this after every iteration of the game. At the end of each user's turn.
+        const availablePaths = findAvailablePath(InitialMatrix);
+        console.log(availablePaths);
+    }
+    console.timeEnd('StartAndFinish'); // end the timer
+
+    const [found, coordinates] = findOccupiedEdge(InitialMatrix);
+    if (found && !Array.isArray(coordinates)) {
+        console.log('Found the occupied edge at', coordinates);
+    }
+
 }
