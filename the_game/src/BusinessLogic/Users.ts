6e9c9aad6bd7bd2abc4929ff5fa541c6dee232f5
Kicky <bucurenciu.cristian@gmail.com>
2023-03-17 19:06:50 +0200

    Started to think of Users.ts

--- the_game/src/BusinessLogic/Users.ts
+++ the_game/src/BusinessLogic/Users.ts
@@ -4,14 +4,17 @@ import {allPaths2} from "./Cards/Paths";
 export const usersList: IUser[] = [
     {
         UserName: 'Player1',
-        Hand: allPaths2.slice(0, 6).map(() => allPaths2[Math.floor(Math.random() * allPaths2.length)])
+        Hand: allPaths2.slice(0, 6).map(() => allPaths2[Math.floor(Math.random() * allPaths2.length)]),
+        Actions: []
     },
     {
         UserName: 'Player2',
-        Hand: allPaths2.slice(0, 6).map(() => allPaths2[Math.floor(Math.random() * allPaths2.length)])
+        Hand: allPaths2.slice(0, 6).map(() => allPaths2[Math.floor(Math.random() * allPaths2.length)]),
+        Actions: []
     },
     {
         UserName: 'Player3',
-        Hand: allPaths2.slice(0, 6).map(() => allPaths2[Math.floor(Math.random() * allPaths2.length)])
+        Hand: allPaths2.slice(0, 6).map(() => allPaths2[Math.floor(Math.random() * allPaths2.length)]),
+        Actions: []
     },
 ]
