aaeaf74cf27f967cafd2e19ae0f8fc2c0c8b7d84
Kicky <bucurenciu.cristian@gmail.com>
2023-03-19 14:08:57 +0200

    Sockets are working from a jsonFile.

--- the_game/pages/Socket/index.jsx
+++ the_game/pages/Socket/index.jsx
@@ -1,39 +1,58 @@
-import { DefaultEventsMap } from '@socket.io/component-emitter';
-import {useEffect, useState} from 'react'
-import io, {Socket} from 'socket.io-client'
-let socket;
+import {useEffect, useState} from 'react';
+import io from 'socket.io-client';
+
 
 const Home = () => {
-  const [input, setInput] = useState('')
-
-  // @ts-ignore
-  useEffect(() => socketInitializer, [])
-
-  const socketInitializer = async () => {
-    await fetch('/api/socket');
-    socket = io()
-
-    socket.on('connect', () => {
-      console.log('connected')
-    })
-
-    socket.on('update-input', msg => {
-      setInput(msg)
-    })
-  }
-
-  const onChangeHandler = (e) => {
-    setInput(e.target.value)
-    socket.emit('input-change', e.target.value)
-  }
-
-  return (
-    <input
-      placeholder="Type something"
-      value={input}
-      onChange={onChangeHandler}
-    />
-  )
-}
+    const [input, setInput] = useState('');
+    const [jsonData, setJsonData] = useState('');
+    
+    // @ts-ignore
+    let socket = null;
+    
+    useEffect(() => {
+        socketInitializer();
+    }, []);
+    
+    const socketInitializer = async () => {
+        await fetch('/api/socket');
+        socket = io();
+        
+        socket.on('connect', () => {
+            console.log('Socket.io connection established.');
+        });
+        
+        socket.on('update-input', (msg) => {
+            setInput(msg);
+        });
+        
+        socket.on('update-json-data', (data) => {
+            setJsonData(JSON.parse(data));
+        });
+    };
+    
+    const onChangeHandler = (e) => {
+        setInput(e.target.value);
+        socket.emit('input-change', e.target.value);
+        
+        // Update jsonData and emit the modified version to the clients
+        const updatedJsonData = {
+            ...jsonData,
+            text: e.target.value,
+        };
+        setJsonData(updatedJsonData);
+        socket.emit('json-data-change', JSON.stringify(updatedJsonData));
+    };
+    
+    return (
+        <>
+            <input
+                placeholder="Type something"
+                value={input}
+                onChange={onChangeHandler}
+            />
+            <p>{JSON.stringify(jsonData, null, 2)}</p>
+        </>
+    );
+};
 
 export default Home;
--- the_game/pages/api/socket.js
+++ the_game/pages/api/socket.js
@@ -1,20 +1,36 @@
-import { Server } from 'socket.io'
+import {Server} from 'socket.io'
+import fs from 'fs';
+import path from 'path';
 
 const SocketHandler = (req, res) => {
-  if (res.socket.server.io) {
-    console.log('Socket is already running')
-  } else {
-    console.log('Socket is initializing')
-    const io = new Server(res.socket.server)
-    res.socket.server.io = io
-
-    io.on('connection', socket => {
-      socket.on('input-change', msg => {
-        socket.broadcast.emit('update-input', msg)
-      })
-    })
-  }
-  res.end()
+    if (res.socket.server.io) {
+        console.log('Socket is already running')
+    } else {
+        console.log('Socket is initializing')
+        const io = new Server(res.socket.server)
+        res.socket.server.io = io
+        const jsonFilePath = path.join(process.cwd(), 'data', 'example.json');
+        
+        io.on('connection', socket => {
+            socket.on('input-change', msg => {
+                socket.broadcast.emit('update-input', msg)
+            })
+            // Send initial JSON data to the client
+            const initialJsonData = fs.readFileSync(jsonFilePath, 'utf8');
+            socket.emit('update-json-data', initialJsonData);
+            socket.on('json-data-change', msg => {
+                socket.broadcast.emit('update-json-data', msg)
+            })
+        })
+        // Watch for changes in the JSON file and emit the updated content to clients
+        fs.watch(jsonFilePath, (eventType, filename) => {
+            if (eventType === 'change') {
+                const updatedJsonData = fs.readFileSync(jsonFilePath, 'utf8');
+                io.emit('update-json-data', updatedJsonData);
+            }
+        });
+    }
+    res.end()
 }
 
 export default SocketHandler
