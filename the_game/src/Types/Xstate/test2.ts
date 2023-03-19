ce8b3a15c37ed677edf9fb1f1aec45c826a05cf2
Kicky <bucurenciu.cristian@gmail.com>
2023-03-20 01:28:10 +0200

    Trying Xstate for my app.

---
+++ the_game/src/Types/Xstate/test2.ts
@@ -0,0 +1,45 @@
+import {createMachine} from 'xstate';
+
+const saboteurMachine = createMachine({
+    id: 'saboteur',
+    initial: 'setup',
+    states: {
+        setup: {
+            on: {
+                SETUP_COMPLETE: 'playRound'
+            }
+        },
+        playRound: {
+            initial: 'startTurn',
+            states: {
+                startTurn: {
+                    on: {
+                        NEXT_PLAYER: 'playerAction'
+                    }
+                },
+                playerAction: {
+                    on: {
+                        PLAY_PATH_CARD: 'endTurn',
+                        PLAY_ACTION_CARD: 'endTurn',
+                        PASS: 'endTurn'
+                    }
+                },
+                endTurn: {
+                    on: {
+                        ROUND_CONTINUES: 'startTurn',
+                        ROUND_END: '#saboteur.roundEnd'
+                    }
+                }
+            }
+        },
+        roundEnd: {
+            on: {
+                LAST_ROUND: 'gameEnd',
+                NEXT_ROUND: 'playRound'
+            }
+        },
+        gameEnd: {
+            type: 'final'
+        }
+    }
+});
