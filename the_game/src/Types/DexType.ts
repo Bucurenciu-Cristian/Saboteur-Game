3631bf72649f663a404bd99c41da23ec9695abd4
Kicky <bucurenciu.cristian@gmail.com>
2023-03-26 00:39:12 +0200

    Some types

--- the_game/src/Types/DexType.ts
+++ the_game/src/Types/DexType.ts
@@ -1,67 +1,65 @@
-import {FOT} from "../BusinessLogic/Cards/Actions";
-import {StaticImageData} from "next/image";
-import {Modes} from "../constants";
+import { StaticImageData } from 'next/image';
+import { FOT } from '../BusinessLogic/Cards/Actions';
+import { Modes } from '../enums';
+import { ActionCardsTypes } from './Players';
 
 export interface dexType {
-    Base: 'B';
-    Path: "P";
-    N: typeof FOT;
-    S: typeof FOT;
-    E: typeof FOT;
-    W: typeof FOT;
-    C: typeof FOT;
-    R: typeof FOT;
-    Gold: 'G';
-    Rock: 'R';
-    Start: 'S';
+  Base: 'B';
+  Path: 'P';
+  N: typeof FOT;
+  S: typeof FOT;
+  E: typeof FOT;
+  W: typeof FOT;
+  C: typeof FOT;
+  R: typeof FOT;
+  Gold: 'G';
+  Rock: 'R';
+  Start: 'S';
 
-    Axe: typeof FOT;
-    Cart: typeof FOT;
-    Lantern: typeof FOT;
+  Axe: typeof FOT;
+  Cart: typeof FOT;
+  Lantern: typeof FOT;
 
-    AxeCart: 'G';
-    AxeLantern: 'B';
-    CartLantern: 'E';
-    AxeS: 'A',
-    CartS: 'C',
-    LanternS: 'L',
-    Action: 'A';
-    Destroy: 'D';
-    Map: 'M';
-    Miner: "M";
-    Player: "L";
-    Reward: 'R';
-    Saboteur: 'S';
+  AxeCart: 'G';
+  AxeLantern: 'B';
+  CartLantern: 'E';
+  AxeS: 'A';
+  CartS: 'C';
+  LanternS: 'L';
+  Action: 'A';
+  Destroy: 'D';
+  Map: 'M';
+  Miner: 'M';
+  Player: 'L';
+  Reward: 'R';
+  Saboteur: 'S';
 }
 
 export interface FOTType {
-    F: 'F';
-    T: 'T'
+  F: 'F';
+  T: 'T';
 }
 
 export interface IMatrix {
-    Card: ICardBasic;
-    Occupied: boolean;
+  Card: ICardBasic;
+  Occupied: boolean;
 }
 
 export interface ICardBasic {
-    code: CharTuple | string;
-    src: StaticImageData;
+  code: CharTuple | string;
+  src: StaticImageData;
 }
 
 export interface ISpecialPath extends ICardBasic {
-    back?: StaticImageData;
+  back?: StaticImageData;
 }
 
-
 export type CharTuple = [
-    first: Modes.Base | Modes.Expansion,
-    second: Modes.Path | Modes.Player | Modes.Action | Modes.Reward,
-    // third?: 'T' | 'F' | 'M' | 'S' | 'E' | 'G' | dex.Base | 'D'| 'C',
-    ...rest: string[]
+  first: Modes.Base | Modes.Expansion,
+  second: Modes.Path | Modes.Player | Modes.Action | Modes.Reward,
+  // third?: 'T' | 'F' | 'M' | 'S' | 'E' | 'G' | dex.Base | 'D'| 'C',
+  ...rest: string[]
 ];
-export type ActionCardCode = CharTuple & { 1: Modes.Action };
-export type ActionCard = ICardBasic & { code: ActionCardCode };
 
 type PathCardCode = CharTuple & { 1: Modes.Path };
 type NorthPart = CharTuple & { 2: Modes.True | Modes.False };
@@ -73,13 +71,20 @@ type RotationPart = CharTuple & { 7: Modes.True | Modes.False };
 type FinalPart = CharTuple & { 8: Modes.Gold | Modes.Rock };
 type FinalCardOptional = CharTuple | FinalPart;
 
+export type PathCard = ICardBasic & {
+  code: PathCardCode & NorthPart & EastPart & SouthPart & WestPart & CenterPart & RotationPart & FinalCardOptional;
+};
 
-export type PathCard =
-    ICardBasic
-    & { code: PathCardCode & NorthPart & EastPart & SouthPart & WestPart & CenterPart & RotationPart & FinalCardOptional };
+export type ActionCardCode = CharTuple & { 1: Modes.Action };
+export type ActionCardCodeThird = CharTuple & { 2: ActionCardsTypes };
+export type ActionCardCodeFour = CharTuple & { 3: Modes.True | Modes.False };
+export type ActionCardCodeOptional = CharTuple | ActionCardCodeFour;
+
+export type ActionCard = ICardBasic & { code: ActionCardCode & ActionCardCodeThird & ActionCardCodeOptional };
 
 export type RewardCardCode = CharTuple & { 1: Modes.Reward };
 export type RewardCard = ICardBasic & { code: RewardCardCode };
 
 export type PlayerCardCode = CharTuple & { 1: Modes.Player };
-export type PlayerCard = ICardBasic & { code: PlayerCardCode };
+export type PlayerCardCodeSecond = CharTuple & { 2: Modes.Miner | Modes.Saboteur };
+export type PlayerCard = ICardBasic & { code: PlayerCardCode & PlayerCardCodeSecond };
--- the_game/src/Types/Players.ts
+++ the_game/src/Types/Players.ts
@@ -1,34 +1,49 @@
-import {ActionCard, ICardBasic} from "./DexType";
+import { ICardBasic } from './DexType';
+import { Modes } from '../enums';
+
+const { Miner, Saboteur } = Modes;
 
 export interface IUser {
-    id?: string | number;
-    userName: string;
-    Hand: ICardBasic[];
-    actions: ActionCard[];
-    selectedCardIndex?: number;
-    score?: number;
-    isTurn?: boolean;
+  id?: string;
+  username: string;
+  role: typeof Miner | typeof Saboteur;
+  goldNuggets?: number;
+  hand: ICardBasic[]; // Assuming you have a Card interface or class
+  blocks: Block[]; // Assuming you have a Block interface or class
+  actions: PlayerAction[]; // List of actions performed by the player
+  selectedCardIndex?: number;
+  score?: number;
+  isTurn?: boolean;
 }
 
-interface Player {
-    id: string;
-    username: string;
-    role: 'Miner' | 'Saboteur';
-    goldNuggets: number;
-    hand: ICardBasic[]; // Assuming you have a Card interface or class
-    blocks: Block[]; // Assuming you have a Block interface or class
-    actions: Action[]; // List of actions performed by the player
+export type ActionCardsTypes =
+  | Modes.Axe
+  | Modes.Lantern
+  | Modes.Cart
+  | Modes.AxeAndLantern
+  | Modes.AxeAndCart
+  | Modes.LanternAndCard
+  | Modes.Destroy
+  | Modes.Map;
+export type ActionCardsTypesBlocks =
+  | Modes.Axe
+  | Modes.Lantern
+  | Modes.Cart
+  | Modes.AxeAndLantern
+  | Modes.AxeAndCart
+  | Modes.LanternAndCard;
+
+export interface Block {
+  type: ActionCardsTypesBlocks;
+  appliedBy: IUser;
+  targetPlayer: IUser;
 }
 
-interface Block {
-    type: 'CartOff' | 'PickaxeOff' | 'LanternOff';
-    appliedBy: Player;
-    targetPlayer: Player;
-}
+type PlayableActions = Modes.playPath | Modes.Discard | Modes.playAction;
 
-interface Action {
-    type: 'PlayCard' | 'DiscardCard' | 'ApplyBlock' | 'RemoveBlock';
-    player: Player;
-    targetPlayer?: Player; // For block-related actions
-    card: Card;
+interface PlayerAction {
+  type: PlayableActions;
+  player: IUser;
+  targetPlayer?: IUser; // For block-related actions
+  card: ICardBasic;
 }
