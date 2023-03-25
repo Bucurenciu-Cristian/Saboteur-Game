0fbe84b02d036c7aa2d95c00b516eca434203c11
Kicky <bucurenciu.cristian@gmail.com>
2023-03-26 00:17:50 +0200

    Created enums.ts

---
+++ the_game/src/enums.ts
@@ -0,0 +1,50 @@
+export enum findCardActions {
+  Start = 'Start',
+  Final = 'Final',
+}
+
+export enum NeighboursActions {
+  ONE = 'ONE',
+  ALL = 'ALL',
+  DIRECTIONS = 'DIRECTIONS',
+}
+
+export enum conDirections {
+  NORTH = 'NORTH',
+  EAST = 'EAST',
+  SOUTH = 'SOUTH',
+  WEST = 'WEST',
+  CENTER = 'CENTER',
+  FALSE = 'FALSE',
+}
+
+export enum Modes {
+  Path = 'P',
+  Player = 'L',
+  Reward = 'R',
+  Action = 'A',
+  Base = 'B',
+  Expansion = 'E',
+  True = 'T',
+  False = 'F',
+  Gold = 'G',
+  Rock = 'R',
+  Destroy = 'D',
+  Map = 'M',
+  Lantern = 'L',
+  Cart = 'C',
+  Axe = 'A',
+  AxeAndCart = 'G',
+  AxeAndLantern = 'B',
+  LanternAndCard = 'E',
+  Miner = 'M',
+  Saboteur = 'S',
+  playPath = 'PlayPath',
+  Discard = 'DiscardPath',
+  playAction = 'PlayAction',
+  Start = 'S',
+}
+
+/*
+ * ([DMGEB]|[LAC][FT])
+ * */
