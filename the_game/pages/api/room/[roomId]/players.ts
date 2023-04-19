57c2aba4fa30fe78ce5bcc3f56fbbce488e877c9
Kicky <bucurenciu.cristian@gmail.com>
2023-04-19 11:52:05 +0300

    Players endpoint now selects only the stuff that I need on the server. I check to see if there are players in that room.

--- the_game/pages/api/room/[roomId]/players.ts
+++ the_game/pages/api/room/[roomId]/players.ts
@@ -8,8 +8,19 @@ export default async (req, res) => {
     try {
       const players = await prisma.user.findMany({
         where: { roomId: parseInt(roomId, 10) },
+        select: {
+          birthDate: true,
+          email: true,
+          roomId: true,
+          id: true,
+          username: true,
+        },
       });
-
+      if (players.length === 0) {
+        players.push({ roomExists: false });
+      } else {
+        players.push({ roomExists: true });
+      }
       res.status(200).json(players);
     } catch (error) {
       res.status(500).json({ message: 'Error fetching players', error });
